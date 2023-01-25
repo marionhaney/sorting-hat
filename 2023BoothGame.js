88ec4faff3b246bba527c368eeda8e9330154870
Marion <marion.haney@gmail.com>
2023-01-24 22:06:52 -0500

    Added session data management for one session

--- 2023BoothGame.js
+++ 2023BoothGame.js
@@ -2,128 +2,138 @@
 // Maddie Hagar, Marion Haney, Spring 2023 Harry Potter Booth Game
 
 const questions = [
-    {   text: "You and two friends need to cross a bridge guarded by a river troll who insists on fighting one of you before he will let all of you pass. Do you:",
+    {
+        text: "You and two friends need to cross a bridge guarded by a river troll who insists on fighting one of you before he will let all of you pass. Do you:",
         answers: [
-            { text: 'Volunteer to fight?',                                                               image: "images/harry.jpg"},
-            { text: 'Attempt to confuse the troll into letting all three of you pass without fighting?', image: "images/harry.jpg"},
-            { text: 'Suggest that all three of you fight?',                                              image: "images/harry.jpg"},
-            { text: 'Suggest drawing straws to decide which of you will fight?',                         image: "images/harry.jpg"}
+            { text: 'Volunteer to fight?', image: "images/harry.jpg" },
+            { text: 'Attempt to confuse the troll into letting all three of you pass without fighting?', image: "images/harry.jpg" },
+            { text: 'Suggest that all three of you fight?', image: "images/harry.jpg" },
+            { text: 'Suggest drawing straws to decide which of you will fight?', image: "images/harry.jpg" }
         ]
     },
-    {   text: "Which of the following would you most like to study?",
+    {
+        text: "Which of the following would you most like to study?",
         answers: [
-            { text: 'Merpeople' , image: "images/harry.jpg"},
-            { text: 'Centaurs'  , image: "images/harry.jpg"},
-            { text: 'Werewolves', image: "images/harry.jpg"},
-            { text: 'Vampires'  , image: "images/harry.jpg"}
+            { text: 'Merpeople', image: "images/harry.jpg" },
+            { text: 'Centaurs', image: "images/harry.jpg" },
+            { text: 'Werewolves', image: "images/harry.jpg" },
+            { text: 'Vampires', image: "images/harry.jpg" }
         ]
     },
-    {   text: "How would you like to be known to history?",
+    {
+        text: "How would you like to be known to history?",
         answers: [
-            { text: "The Bold" , image: "images/harry.jpg"},
-            { text: "The Good" , image: "images/harry.jpg"},
-            { text: "The Wise" , image: "images/harry.jpg" },
-            { text: "The Great", image: "images/harry.jpg"}
-       ]
-    },
-    {   text: "What is your favorite CMU tradition?",
-        answers: [
-            { text: 'Painting the Fence', image: "images/harry.jpg"},
-            { text: 'Buggy',              image: "images/harry.jpg"},
-            { text: 'Booth',              image: "images/harry.jpg"},
-            { text: 'O-Week',             image: "images/harry.jpg"}
+            { text: "The Bold", image: "images/harry.jpg" },
+            { text: "The Good", image: "images/harry.jpg" },
+            { text: "The Wise", image: "images/harry.jpg" },
+            { text: "The Great", image: "images/harry.jpg" }
         ]
     },
-    {   text: "Which of these most accurately describes your relationship with your closest friends?",
+    {
+        text: "What is your favorite CMU tradition?",
         answers: [
-            { text: 'I love surrounding myself with people – the more friends I have, the better!', image: "images/harry.jpg"},
-            { text: 'I have a few very close friends that I would trust with my life.',             image: "images/harry.jpg"},
-            { text: 'I tend to be wary around new people, so I do not make new friends often.',     image: "images/harry.jpg"},
-            { text: 'I find myself becoming friends with people who can help me to succeed.',       image: "images/harry.jpg"}
+            { text: 'Painting the Fence', image: "images/harry.jpg" },
+            { text: 'Buggy', image: "images/harry.jpg" },
+            { text: 'Booth', image: "images/harry.jpg" },
+            { text: 'O-Week', image: "images/harry.jpg" }
         ]
     },
-    {   text: "The first Quidditch match of the season is approaching, and you can't wait to get involved. What role are you playing?",
+    {
+        text: "Which of these most accurately describes your relationship with your closest friends?",
         answers: [
-            { text: 'Seeker. I want the glory!',                                     image: "images/harry.jpg"},
-            { text: 'Chaser. I like to be involved and work as part of the team.',   image: "images/harry.jpg"},
-            { text: 'Beater. I like having all that power.',                         image: "images/harry.jpg"},
-            { text: 'I will be in the crowd, making sure supporter morale is high!', image: "images/harry.jpg"}
+            { text: 'I love surrounding myself with people – the more friends I have, the better!', image: "images/harry.jpg" },
+            { text: 'I have a few very close friends that I would trust with my life.', image: "images/harry.jpg" },
+            { text: 'I tend to be wary around new people, so I do not make new friends often.', image: "images/harry.jpg" },
+            { text: 'I find myself becoming friends with people who can help me to succeed.', image: "images/harry.jpg" }
         ]
     },
-    {   text: "Which of your skills are you most proud of?",
+    {
+        text: "The first Quidditch match of the season is approaching, and you can't wait to get involved. What role are you playing?",
         answers: [
-            { text: 'My ability to absorb new information.', image: "images/harry.jpg"},
-            { text: 'My ability to make new friends.',       image: "images/harry.jpg"},
-            { text: 'My ability to get what I want.',        image: "images/harry.jpg"},
-            { text: 'My ability to keep secrets.',           image: "images/harry.jpg"}
+            { text: 'Seeker. I want the glory!', image: "images/harry.jpg" },
+            { text: 'Chaser. I like to be involved and work as part of the team.', image: "images/harry.jpg" },
+            { text: 'Beater. I like having all that power.', image: "images/harry.jpg" },
+            { text: 'I will be in the crowd, making sure supporter morale is high!', image: "images/harry.jpg" }
         ]
     },
-    {   text: "You have a huge exam to study for, and your common room is too loud. Where do you go?",
+    {
+        text: "Which of your skills are you most proud of?",
         answers: [
-            { text: 'Gates',            image: "images/harry.jpg"},
-            { text: 'Tepper',           image: "images/harry.jpg"},
-            { text: 'Sorrells Library', image: "images/harry.jpg"},
-            { text: 'The UC',           image: "images/harry.jpg"}
+            { text: 'My ability to absorb new information.', image: "images/harry.jpg" },
+            { text: 'My ability to make new friends.', image: "images/harry.jpg" },
+            { text: 'My ability to get what I want.', image: "images/harry.jpg" },
+            { text: 'My ability to keep secrets.', image: "images/harry.jpg" }
         ]
     },
-    {   text: "Which of these magical events would you most like to experience?",
+    {
+        text: "You have a huge exam to study for, and your common room is too loud. Where do you go?",
         answers: [
-            { text: 'The Triwizard Tournament', image: "images/harry.jpg"},
-            { text: 'The Quidditch World Cup',  image: "images/harry.jpg"},
-            { text: 'The Yule Ball',            image: "images/harry.jpg"},
-            { text: 'Christmas at Hogwarts',    image: "images/harry.jpg"}
+            { text: 'Gates', image: "images/harry.jpg" },
+            { text: 'Tepper', image: "images/harry.jpg" },
+            { text: 'Sorrells Library', image: "images/harry.jpg" },
+            { text: 'The UC', image: "images/harry.jpg" }
         ]
     },
-    {   text: "Which path do you take?",
+    {
+        text: "Which of these magical events would you most like to experience?",
         answers: [
-            { text: 'Twisting leafy path through the woods',             image: "images/harry.jpg"},
-            { text: 'A dark, lantern-lit alley',                         image: "images/harry.jpg"},
-            { text: 'A wide, sunny, grassy path',                        image: "images/harry.jpg"},
-            { text: 'A cobblestone street lined with ancient buildings', image: "images/harry.jpg"}
-        ]    
+            { text: 'The Triwizard Tournament', image: "images/harry.jpg" },
+            { text: 'The Quidditch World Cup', image: "images/harry.jpg" },
+            { text: 'The Yule Ball', image: "images/harry.jpg" },
+            { text: 'Christmas at Hogwarts', image: "images/harry.jpg" }
+        ]
+    },
+    {
+        text: "Which path do you take?",
+        answers: [
+            { text: 'Twisting leafy path through the woods', image: "images/harry.jpg" },
+            { text: 'A dark, lantern-lit alley', image: "images/harry.jpg" },
+            { text: 'A wide, sunny, grassy path', image: "images/harry.jpg" },
+            { text: 'A cobblestone street lined with ancient buildings', image: "images/harry.jpg" }
+        ]
     }
 ]
 
 const results = [
-    { house: "Gryffindor", code: "GRY", image: "images/harry.png", desc: "Gryffindor house is where you would find the pluckiest and most daring students (there's a reason the house symbol is the brave lion). The house colours are scarlet and gold, the common room lies up in Gryffindor Tower and the Head of House is Professor Minerva McGonagall."},
-    { house: "Hufflepuff", code: "HUF", image: "images/harry.jpg", desc: "Hufflepuff is the most inclusive among the four houses—valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its members. The emblematic animal is a badger, and yellow and black are its colours."},
-    { house: "Ravenclaw" , code: "RAV", image: "images/harry.jpg" , desc: "Ravenclaws possess the traits of cleverness, wisdom, wit, intellectual ability and creativity. According to Slytherin prefect Gemma Farley, Ravenclaws are so competitive when it comes to academic success that they are known to back stab each other, and likely other students, in order to get top marks."},
-    { house: "Slytherin" , code: "SLY", image: "images/harry.jpg" , desc: "Each house has a set of traits and characteristics associated with it, and those in Slytherin are known for being ambitious, cunning, and resourceful. Slytherins are also sometimes regarded as being evil thanks to the fact that many of the most sinister witches and wizards have been associated with this house."}
+    { house: "Gryffindor", code: "GRY", image: "images/harry.png", desc: "Gryffindor house is where you would find the pluckiest and most daring students (there's a reason the house symbol is the brave lion). The house colours are scarlet and gold, the common room lies up in Gryffindor Tower and the Head of House is Professor Minerva McGonagall." },
+    { house: "Hufflepuff", code: "HUF", image: "images/harry.jpg", desc: "Hufflepuff is the most inclusive among the four houses—valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its members. The emblematic animal is a badger, and yellow and black are its colours." },
+    { house: "Ravenclaw", code: "RAV", image: "images/harry.jpg", desc: "Ravenclaws possess the traits of cleverness, wisdom, wit, intellectual ability and creativity. According to Slytherin prefect Gemma Farley, Ravenclaws are so competitive when it comes to academic success that they are known to back stab each other, and likely other students, in order to get top marks." },
+    { house: "Slytherin", code: "SLY", image: "images/harry.jpg", desc: "Each house has a set of traits and characteristics associated with it, and those in Slytherin are known for being ambitious, cunning, and resourceful. Slytherins are also sometimes regarded as being evil thanks to the fact that many of the most sinister witches and wizards have been associated with this house." }
 ]
 
-const keyCodes     = ['49', '50', '51', '52']
-const numHouses    = 4
+const keyCodes = ['49', '50', '51', '52']
+const numHouses = 4
 const numQuestions = 10
-const maxIdleTime  = 59
-const minIdLength  = 2
-const enter        = '13'
-const space        = '32'
-const restart      = '48'
-const countString  = "/".concat(numQuestions.toString())
+const maxIdleTime = 59
+const minIdLength = 2
+const enter = '13'
+const space = '32'
+const restart = '48'
+const countString = "/".concat(numQuestions.toString())
 
 var sessionID = ""
-var userName  = ""
-var result    = ""
-var started   = false
-var finished  = false
-var idle      = false
-var idleTime  = 0
+var userName = ""
+var result = ""
+var started = false
+var finished = false
+var idle = false
+var idleTime = 0
 var currentQuestion = 0
-var chosenAnswers   = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-var question        = questions[currentQuestion]
-var questionBlock   = document.createElement('div')
-var questionHeading = document.createElement('h2' )
-var answersBlock    = document.createElement('div')
-var counter         = document.createElement('div')
-var session         = document.createElement('div')
-var startScreen     = document.createElement('div')
-var idleScreen      = document.createElement('div')
-var reset           = document.createElement('top-caption')
-var images          = document.getElementsByTagName("img");
-var titleDisplay    = document.querySelector('#titles')
+var chosenAnswers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+var question = questions[currentQuestion]
+var questionBlock = document.createElement('div')
+var questionHeading = document.createElement('h2')
+var answersBlock = document.createElement('div')
+var counter = document.createElement('div')
+var session = document.createElement('div')
+var startScreen = document.createElement('div')
+var idleScreen = document.createElement('div')
+var reset = document.createElement('top-caption')
+var images = document.getElementsByTagName("img");
+var titleDisplay = document.querySelector('#titles')
 var questionDisplay = document.querySelector('#questions')
-var answerDisplay   = document.querySelector('#answer')
-var formDisplay     = document.querySelector('#form')
+var answerDisplay = document.querySelector('#answer')
+var formDisplay = document.querySelector('#form')
 
 document.addEventListener("keyup", handleKeyEvent)
 idleScreen.classList.add('idle')
@@ -148,20 +158,20 @@ function incIdleTime() {
 function checkIdleTime() {
     if (idleTime >= maxIdleTime && !finished) {
         showIdleScreen()
-    } 
+    }
 }
 
 function resetGlobals() {
     sessionID = ""
-    userName  = ""
-    result    = ""
-    started   = false
-    finished  = false
-    idle      = false
-    idleTime  = 0
+    userName = ""
+    result = ""
+    started = false
+    finished = false
+    idle = false
+    idleTime = 0
     currentQuestion = 0
-    chosenAnswers   = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    question        = questions[currentQuestion]
+    chosenAnswers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    question = questions[currentQuestion]
     document.getElementById('name_entry').value = ''
 }
 
@@ -253,7 +263,7 @@ function populateQuestion(index) {
     questionBlock.id = currentQuestion
     questionBlock.classList.add('question-block')
 
-    counter.textContent = (currentQuestion+1).toString().concat(countString)
+    counter.textContent = (currentQuestion + 1).toString().concat(countString)
     counter.classList.add('counter')
     questionDisplay.append(counter)
 
@@ -268,13 +278,13 @@ function populateQuestion(index) {
         const answer = question.answers[i]
         const answerBlock = document.createElement('div')
         answerBlock.classList.add('answer-block')
-        
+
         const answerImage = document.createElement('img')
         answerImage.setAttribute('src', answer.image)
-        
+
         const answerTitle = document.createElement('h3')
         answerTitle.textContent = answer.text
-       
+
         answerBlock.append(answerImage, answerTitle)
         answersBlock.append(answerBlock)
     }
@@ -304,8 +314,8 @@ function startQuiz() {
 function showNextQuestion(evt) {
     for (var i = 0; i < numHouses; i++) {
         if (evt.keyCode == keyCodes[i]) {
-            const qId  = currentQuestion
-            const ansId = i+1
+            const qId = currentQuestion
+            const ansId = i + 1
             processAnswer(qId, ansId)
             return
         }
@@ -335,7 +345,7 @@ function setSessionID(name) {
     populateQuestion(currentQuestion)
 }
 
-function processAnswer(questionId, answerId){
+function processAnswer(questionId, answerId) {
     chosenAnswers[questionId] = answerId
     currentQuestion++
     resetQuestion()
@@ -343,8 +353,11 @@ function processAnswer(questionId, answerId){
         populateQuestion(currentQuestion)
     } else {
         //showAnswer();
-        // this is the end of the test, save the session data
-    } 
+        // this is the end of the test, analyze the session data
+        console.log("Sorting you into your house... \n");
+        var S = new SessionData(sessionID, userName);
+        analyzeSession(S, chosenAnswers);
+    }
 }
 
 showStart()
--- dataManagement.js
+++ dataManagement.js
@@ -1,4 +1,46 @@
 // Data Management: JS object and functions for data storage and management
+// all globals and helper functions
 // Marion Haney, Spring 2023 Harry Potter Booth Game
 
+// global variable: lookup table for the question/answer point values
+const QALookup = [
+    [[80,5,0,15], [0,10,90,0], [20,0,0,80], [0,90,10,0]],
+    [[15,20,60,5], [5,60,20,15], [75,0,0,25], [20,0,0,80]],
+    [[0,0,0,100], [0,0,100,0], [100,0,0,0], [0,100,0,0]],
+    [[20,10,0,70], [20,10,70,0], [60,5,20,15], [15,75,3,7]],
+    [[0,100,0,0], [100,0,0,0], [0,0,100,0], [0,0,0,100]],
+    [[100,0,0,0], [0,80,20,0], [0,0,0,100], [0,20,80,0]],
+    [[0,0,100,0], [0,100,0,0], [0,0,0,100], [100,0,0,0]],
+    [[0,0,75,25], [75,15,0,10], [0,0,25,75], [15,80,0,5]],
+    [[100,0,0,0], [0,0,0,100], [0,0,100,0], [0,100,0,0]],
+    [[70,0,0,30], [30,0,0,70], [0,100,0,0], [0,0,100,0]]
+];
+
+
+// class for session data-- one instance is one person's session
+class SessionData {
+    constructor(sessionID, name) {
+        this.sessionID = sessionID;
+        this.name = name;
+        this.result = {house:"", GRY: 0, HUF: 0, RAV: 0, SLY:0};
+    }
+  }
+
+
+// function for analyzing the results, takes in a SessionData object and the array of chosen answers
+function analyzeSession(S, chosenAnswers) {
+    for (let i = 0; i <= 9; i++) {
+        answer = chosenAnswers[i];
+        vals = QALookup[i, answer];
+        this.result.GRY += vals[0];
+        this.result.HUF += vals[1];
+        this.result.RAV += vals[2];
+        this.result.SLY += vals[3];
+    }
+    let endSums = [this.result.GRY, this.result.HUF, this.result.RAV, this.result.SLY];
+    let houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+    let whichMax = endSums.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+    this.house = houses[whichMax];
+    console.log("You are " + this.house + " !");
+}
 
