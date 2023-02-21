c8df78385a0098b0206c80bd06d15c1c263d6ad6
Marion <marion.haney@gmail.com>
2023-02-20 19:45:09 -0500

    Node.js adaptation

--- server.js
+++ server.js
@@ -2,13 +2,25 @@
 
 // Load Node modules
 var express = require('express');
+const ejs = require('ejs');
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
 
-// http://localhost:8080/2023BoothFormat.html
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('pages/2023BoothFormat'); // game ejs file
+});
+
+
+// NOTES:
+// http://localhost:8080
 // npm start
 // Ctrl + C to kill server.js
+// installing packages to package.json: npm install PACKAGE_NAME --save
