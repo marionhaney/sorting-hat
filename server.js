752eaf31140d4c39c0ffc354abb77b9970d80e0f
Marion <marion.haney@gmail.com>
2023-02-20 20:25:35 -0500

    HTML file running on node.js server

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-// launch the server and run the files in the public folder
-
+// launch the server and run the files in the views folder
 // Load Node modules
 var express = require('express');
-const ejs = require('ejs');
 // Initialise Express
 var app = express();
 // Render static files
-app.use(express.static('public'));
-// Set the view engine to ejs
-app.set('view engine', 'ejs');
+app.use(express.static('game'));
 // Port website will run on
 app.listen(8080);
 
-// *** GET Routes - display pages ***
-// Root Route
-app.get('/', function (req, res) {
-    res.render('pages/2023BoothFormat'); // game ejs file
-});
-
-
 // NOTES:
-// http://localhost:8080
+// http://localhost:8080/2023BoothFormat.html
 // npm start
 // Ctrl + C to kill server.js
 // installing packages to package.json: npm install PACKAGE_NAME --save
