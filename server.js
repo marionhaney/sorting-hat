d0446648fa0910911ba6f69b99a9229975211146
Marion <marion.haney@gmail.com>
2023-04-06 21:38:14 -0400

    Added johnny-five package

--- server.js
+++ server.js
@@ -1,56 +1,7 @@
 // launch the server and run the files in the views folder
+
 // Load Node modules
 var express = require('express');
-
-/**
-// Raspberry Pi Input modules & input listener for LED buttons
-// set each button to the corresponding keypress
-var Gpio = require('onoff').Gpio;
-
-const ans1 = new Gpio( '49', 'in', 'both' ); // 1
-const ans2 = new Gpio( '50', 'in', 'both' ); // 2
-const ans3 = new Gpio( '51', 'in', 'both' ); // 3
-const ans4 = new Gpio( '52', 'in', 'both' ); // 4
-
-// Raspberry Pi button event listener
-// listen for pin voltage change
-ans1.watch( ( err, value ) => {
-    if( err ) {
-      console.log( 'Error', err );
-    }
-  
-    // log pin value (0 or 1)
-    console.log( 'Pin value for Answer 1', value );
-} );
-
-ans2.watch( ( err, value ) => {
-    if( err ) {
-      console.log( 'Error', err );
-    }
-  
-    // log pin value (0 or 1)
-    console.log( 'Pin value for Answer 2', value );
-} );
-
-ans3.watch( ( err, value ) => {
-    if( err ) {
-      console.log( 'Error', err );
-    }
-  
-    // log pin value (0 or 1)
-    console.log( 'Pin value for Answer 3', value );
-} );
-
-ans4.watch( ( err, value ) => {
-    if( err ) {
-      console.log( 'Error', err );
-    }
-  
-    // log pin value (0 or 1)
-    console.log( 'Pin value for Answer 4', value );
-} );
-*/
-
 // Initialise Express
 var app = express();
 // Render static files
