45fb195e1487be28390c42b00d602913a66950c0
Marion <marion.haney@gmail.com>
2023-04-07 18:46:13 -0400

    button arduino

--- game/arduino/buttons.ino
+++ game/arduino/buttons.ino
@@ -1,33 +1,52 @@
 // you can ignore all the files other than this one
 
+#define DELAY 200
+
 #define WHITE 13
 #define BLUE 12
 #define GREEN 11
 #define YELLOW 10
 
-void setup() {
-    pinMode(WHITE, INPUT);
-    pinMode(BLUE, INPUT);
-    pinMode(GREEN, INPUT);
-    pinMode(YELLOW, INPUT);
+bool was_high;
 
-    Serial.begin(9600);
+void setup() {
+  pinMode(WHITE, INPUT);
+  pinMode(BLUE, INPUT);
+  pinMode(GREEN, INPUT);
+  pinMode(YELLOW, INPUT);
+
+  was_high = false;
+
+  Serial.begin(9600);
 }
 
 void loop() {
-    if (digitalRead(WHITE)) {
-        Serial.println("WHITE");
+  if (digitalRead(WHITE)) {
+    if (!was_high){
+      Serial.println("WHITE");
+      delay(DELAY);
+  }
+    was_high = true;
+  } else if (digitalRead(BLUE)) {
+    if (!was_high){
+      Serial.println("BLUE");
+      delay(DELAY);
     }
-
-    if (digitalRead(BLUE)) {
-        Serial.println("BLUE");
+    was_high = true;
+  } else if (digitalRead(GREEN)) {
+    if (!was_high){
+      Serial.println("GREEN");
+      delay(DELAY);
     }
-
-    if (digitalRead(GREEN)) {
-        Serial.println("GREEN");
+    was_high = true;
+  } else if (digitalRead(YELLOW)) {
+    if (!was_high){
+      Serial.println("YELLOW");
+      delay(DELAY);
     }
-
-    if (digitalRead(YELLOW)) {
-        Serial.println("YELLOW");
-    }
+    was_high = true;
+  } else {
+    //Serial.println("LOW");
+    was_high = false;
+  }
 }
